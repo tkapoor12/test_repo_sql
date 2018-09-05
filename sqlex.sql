c969deda23327515cd295013d4403c82693c490e
tkapoor12 <42993572+tkapoor12@users.noreply.github.com>
2018-09-05 10:17:24 +0000

    Update sqlex.sql
    sql change 1

--- sqlex.sql
+++ sqlex.sql
@@ -1,2 +1,2 @@
 Select * from BMC_CORE_BMC_BaseElement
-where classid='BMC_ComputerSystem' and datasetid='BMC.ASSET'
+where classid='BMC_ComputerSystem' and datasetid='BMC.ASSET' and MarkAsdeleted='Yes'
